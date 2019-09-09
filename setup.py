fc961ee2689537908a502c56a66b270e81098874
leoyong <42500958+leoyong@users.noreply.github.com>
2019-09-09 10:45:03 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="CoordTransfor",
+    version="0.1.0",
+    description="The package is mainly for coordination transform",
+    author="Aoyong Li",
+    author_email="aoyong.lee@outlook.com",
+    packages=["CoordTransform"]
+)
